9c6f660497cbcffaefa0a3e6e03a3c38c7ef5c74
Cam <cam+gh2@caesium.co.nz>
2023-07-05 23:02:32 +1200

    Drop support for non ed25519 keys.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,48 +2,10 @@
 
 set -euo pipefail
 
-SSH_HOST_RSA_KEY=${SSH_HOST_RSA_KEY:-}
-SSH_HOST_RSA_PUBLIC_KEY=${SSH_HOST_RSA_PUBLIC_KEY:-}
-
-SSH_HOST_ECDSA_KEY=${SSH_HOST_ECDSA_KEY:-}
-SSH_HOST_ECDSA_PUBLIC_KEY=${SSH_HOST_ECDSA_PUBLIC_KEY:-}
-
 SSH_HOST_ED25519_KEY=${SSH_HOST_ED25519_KEY:-}
 SSH_HOST_ED25519_PUBLIC_KEY=${SSH_HOST_ED25519_PUBLIC_KEY:-}
 
-SSH_AUTHORIZED_KEY=${SSH_AUTHORIZED_KEY:-}
-
-if [[ -n "${SSH_HOST_RSA_KEY}" ]]; then
-  echo "${SSH_HOST_RSA_KEY}" >/etc/ssh/ssh_host_rsa_key
-  chown root:root /etc/ssh/ssh_host_rsa_key
-  chmod 0400 /etc/ssh/ssh_host_rsa_key
-  unset SSH_HOST_RSA_KEY
-else
-  ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-
-if [[ -n "${SSH_HOST_RSA_PUBLIC_KEY}" ]]; then
-  echo "${SSH_HOST_RSA_PUBLIC_KEY}" >/etc/ssh/ssh_host_rsa_key.pub
-  chown root:root /etc/ssh/ssh_host_rsa_key.pub
-  chmod 0400 /etc/ssh/ssh_host_rsa_key.pub
-  unset SSH_HOST_RSA_PUBLIC_KEY
-fi
-
-if [[ -n "${SSH_HOST_ECDSA_KEY}" ]]; then
-  echo "${SSH_HOST_ECDSA_KEY}" >/etc/ssh/ssh_host_ecdsa_key
-  chown root:root /etc/ssh/ssh_host_ecdsa_key
-  chmod 0400 /etc/ssh/ssh_host_ecdsa_key
-  unset SSH_HOST_ECDSA_KEY
-else
-  ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-fi
-
-if [[ -n "${SSH_HOST_ECDSA_PUBLIC_KEY}" ]]; then
-  echo "${SSH_HOST_ECDSA_PUBLIC_KEY}" >/etc/ssh/ssh_host_ecdsa_key.pub
-  chown root:root /etc/ssh/ssh_host_ecdsa_key.pub
-  chmod 0400 /etc/ssh/ssh_host_ecdsa_key.pub
-  unset SSH_HOST_ECDSA_PUBLIC_KEY
-fi
+SSH_AUTHORIZED_KEYS=${SSH_AUTHORIZED_KEYS:-}
 
 if [[ -n "${SSH_HOST_ED25519_KEY}" ]]; then
   echo "${SSH_HOST_ED25519_KEY}" >/etc/ssh/ssh_host_ed25519_key
@@ -61,8 +23,8 @@ if [[ -n "${SSH_HOST_ED25519_PUBLIC_KEY}" ]]; then
   unset SSH_HOST_ED25519_PUBLIC_KEY
 fi
 
-if [[ -n "${SSH_AUTHORIZED_KEY}" ]]; then
-  echo "${SSH_AUTHORIZED_KEY}" >/home/publisher/.ssh/authorized_keys
+if [[ -n "${SSH_AUTHORIZED_KEYS}" ]]; then
+  echo "${SSH_AUTHORIZED_KEYS}" >/home/publisher/.ssh/authorized_keys
   chown -R publisher:publisher /home/publisher/.ssh
   chmod 0400 /home/publisher/.ssh/authorized_keys
   unset SSH_AUTHORIZED_KEY
